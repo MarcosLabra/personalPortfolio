21fe147ad80abad61b2510e45fa0eb39d269bb1a
MarcosLabra <mlabra.dev@gmail.com>
2022-07-23 16:47:13 -0300

    footer done

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -1,6 +1,19 @@
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <div className="credits">
+        <p>dev by</p>
+        <h4>Marcos <span>LABRA</span></h4>
+      </div>
+      <div className="social">
+        <a href="https://github.com/MarcosLabra" target="_blank" rel="noreferrer">
+          <img src="/images/social/gitCat.svg" alt="GitHub logo" />
+        </a>
+        <a href="https://www.linkedin.com/in/marcoslabra/" target="_blank" rel="noreferrer">
+          <img src="/images/social/linkedIn.svg" alt="LinkedIn Logo" />
+        </a>
+      </div>
+    </footer>
   )
 }
 
