69b399f4e9a8b0ec464d4c00894e9846ca98fd19
MarcosLabra <mlabra.dev@gmail.com>
2022-07-24 15:35:09 -0300

    modifico cv y agrego hovers

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -3,17 +3,20 @@ const Footer = () => {
   function handleClick() {
     document.querySelector('#mail').classList.toggle('show');
   }
-  
+
   return (
     <footer>
       <div className="credits">
         <p>dev by</p>
         <h4>Marcos <span>LABRA</span></h4>
       </div>
+      <div className="copyright">
+        <p>&copy; 2022</p>
+      </div>
       <div className="social">
         <div className="social__mail">
           <p id="mail">mlabra.dev@gmail.com</p>
-          <img src="/images/mail.svg" alt="" onClick={handleClick}/>
+          <img src="/images/mail.svg" alt="" onClick={handleClick} />
         </div>
         <a href="https://github.com/MarcosLabra" target="_blank" rel="noreferrer">
           <img src="/images/social/gitCat.svg" alt="GitHub logo" />
